52d8e871d9448a22102f27956256553681d1755c
lucasdoourado <lucas.ada95@outlook.com>
2023-06-21 22:23:12 -0300

    changes

--- cypress/e2e/signup-login.cy.ts
+++ cypress/e2e/signup-login.cy.ts
@@ -1,13 +1,31 @@
 describe('signup and login', () => {
   let randomString = Math.random().toString(36).substring(2);
+  let username = 'user' + randomString;
+  let email = 'user_test_' + randomString + '@email.com';
+  const password = 'Test_user1';
   it('signup', () => {
     cy.intercept('POST', '**/*.realworld.io/api/users').as('newUser');
     cy.visit('http://localhost:4200/');
     cy.get('.nav').contains('Sign up').click();
     cy.url().should('include', '/register');
-    cy.get('input[formcontrolname="username"]').type('user' + randomString);
-    cy.get('input[formcontrolname="email"]').type('user_test_' + randomString + '@email.com');
-    cy.get('input[formcontrolname="password"]').type('Test_user1');
+    cy.get('input[formcontrolname="username"]').type(username);
+    cy.get('input[formcontrolname="email"]').type(email);
+    cy.get('input[formcontrolname="password"]').type(password);
     cy.get('button').click();
+    cy.wait('@newUser').then(({request, response}) => {
+      cy.log('Request: ' + JSON.stringify(request));
+      cy.log('Response: ' + JSON.stringify(response));
+      expect(response.statusCode).to.eq(200);
+      expect(request.body.user.username).to.eq(username);
+      expect(request.body.user.email).to.eq(email);
+    });
+  });
+  it('login', () => {
+    cy.visit('http://localhost:4200/');
+    cy.get('.nav').contains('Sign in').click();
+    cy.get('input[formcontrolname="email"]').type(email);
+    cy.get('input[formcontrolname="password"]').type(password);
+    cy.get('button').contains("Sign in").click();
+    cy.get(':nth-child(4) > .nav-link').should('contain', username);
   });
 });
